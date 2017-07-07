9dc1f9b42df87b6f11d9b9ff32e7fd2920a7646c
Andy Culbertson <thinkulum@gmail.com>
2017-07-07 09:54:01 -0500

    Add other empty templates

--- __tests__/app.js
+++ __tests__/app.js
@@ -9,9 +9,27 @@ describe('generator-python-cmd:app', () => {
       .withPrompts({someAnswer: true});
   });
 
+
   it('creates files', () => {
     assert.file([
-      'README.md'
+      '.gitignore',
+      'app.py',
+      'LICENSE.md',
+      'README.md',
+      'setup.py',
+      'package/cli.py',
+      'package/config.py',
+      'package/controller.py',
+      'package/__init__.py',
+      'docs/make.bat',
+      'docs/Makefile',
+      'docs/source/_static',
+      'docs/source/_templates',
+      'docs/source/conf.py',
+      'docs/source/index.rst',
+      'tests/context.py',
+      'tests/test.py'
+      'config',
     ]);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -2,6 +2,7 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
   prompting() {
@@ -23,12 +24,112 @@ module.exports = class extends Generator {
     });
   }
 
+
+  // Adapted from 
+  // https://github.com/yeoman/generator-webapp/blob/master/app/index.js
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('README.md'),
-      this.destinationPath('README.md')
-    );
+    this._writingGit();
+    this._writingScript();
+    this._writingConfig();
+    this._writingLib();
+    this._writingDocs();
+    this._writingTests();
+    this._writingSetup();
   }
+
+
+  _writingGit() {
+    this.fs.copy(
+      this.templatePath('gitignore'),
+      this.destinationPath('.gitignore'));
+  }
+
+
+  _writingScript() {
+    const templatePaths = ['app.py'];
+    const options = {};
+    this._copyTpls(templatePaths, options);
+  }
+
+
+  _writingConfig() {
+    const dirPaths = ['config'];
+    this._makeDirs(dirPaths);
+  }
+
+
+  _writingLib() {
+    const templatePaths = [
+      'package/cli.py',
+      'package/config.py',
+      'package/controller.py',
+      'package/__init__.py'
+    ];
+    const options = {};
+    this._copyTpls(templatePaths, options);
+  }
+
+
+  _writingDocs() {
+    const dirPaths = [
+      'docs/source/_static',
+      'docs/source/_templates'
+    ];
+    this._makeDirs(dirPaths);
+
+    const templatePaths = [
+      'LICENSE.md',
+      'README.md',
+      'docs/make.bat',
+      'docs/Makefile',
+      'docs/source/conf.py',
+      'docs/source/index.rst'
+    ];
+    const options = {};
+    this._copyTpls(templatePaths, options);
+  }
+
+
+  _writingTests() {
+    const templatePaths = [
+      'tests/context.py',
+      'tests/test.py'
+    ];
+    const options = {};
+    this._copyTpls(templatePaths, options);
+  }
+
+
+  _writingSetup() {
+    const templatePaths = ['setup.py'];
+    const options = {};
+    this._copyTpls(templatePaths, options);
+  }
+
+
+  _copyTpls(templatePaths = [], options = {}) {
+    var i;
+    var templatePath;
+    for (i = 0; i < templatePaths.length; i++) {
+      templatePath = templatePaths[i];
+      this.fs.copyTpl(
+        this.templatePath(templatePath),
+        this.destinationPath(templatePath),
+        options
+      );
+    }
+  }
+
+
+  _makeDirs(dirPaths = []) {
+    var i;
+    var dirPath;
+    for (i = 0; i < dirPaths.length; i++) {
+      dirPath = dirPaths[i];
+      mkdirp(dirPath);
+    }
+  }
+
 
   install() {
     this.installDependencies();
