6bdf3e028838cca05ce314e3a0a4c6c4fb045571
Andy Culbertson <thinkulum@gmail.com>
2019-05-15 22:27:07 -0500

    Fix the tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -17,33 +17,61 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 const fs = require('fs');
+const process = require('process');
+
+/**
+ * Return an absolute path converted from the given relative path.
+ *
+ * @param {String}   relPath An relative path.
+ *
+ * @returns {String} The absolute path based on the current working directory.
+ */
+var absPath = function (relPath) {
+  return process.cwd() + '/' + relPath;
+};
+
+/**
+ * Return an Array of absolute paths converted from the given relative paths.
+ *
+ * @param {Array}   relPaths An Array of relative paths.
+ *
+ * @returns {Array} The absolute paths based on the current working directory.
+ */
+var absPaths = function (relPaths) {
+  return relPaths.map(function (relPath) {
+    return absPath(relPath);
+  });
+};
 
 // Adapted from https://gist.github.com/kethinov/6658166
 /**
  * Return an array of files and directories found within dir, including the contents of its subdirectories.
  *
- * @param   {String}         dir      The directory you're listing.
+ * @param   {String}         dir      The relative path of the directory you're listing.
+ * @param   {String}         dirBase  The absolute base for the relative path.
  * @param   {Array.<String>} itemList The list of paths to append to. This should usually be empty.
  * @param   {String}         itemType The type of filesystem item to return: 'all', 'dir', 'emptyDir', or 'file'.
  *
  * @returns {Array.<String>} The list of files and directories under dir.
  */
-var walkSync = function (dir, itemList, itemType) {
-  const theseItems = fs.readdirSync(dir);
+var walkSync = function (dir, dirBase, itemList, itemType) {
+  const dirAbsPath = path.join(dirBase, dir);
+  const theseItems = fs.readdirSync(dirAbsPath);
   itemList = itemList || [];
   if (theseItems.length === 0 && itemType === 'emptyDir') {
     itemList.push(dir);
   }
 
   theseItems.forEach(function (item) {
-    if (fs.statSync(path.join(dir, item)).isDirectory()) {
+    const itemRelPath = path.join(dir, item);
+    if (fs.statSync(path.join(dirAbsPath, item)).isDirectory()) {
       if (itemType === 'all' || itemType === 'dir') {
-        itemList.push(path.join(dir, item));
+        itemList.push(itemRelPath);
       }
 
-      itemList = walkSync(path.join(dir, item), itemList, itemType);
+      itemList = walkSync(itemRelPath, dirBase, itemList, itemType);
     } else if (itemType === 'all' || itemType === 'file') {
-      itemList.push(path.join(dir, item));
+      itemList.push(itemRelPath);
     }
   });
   return itemList;
@@ -115,13 +143,13 @@ describe('generator-python-cmd:app', () => {
   ];
 
   it('creates expected files', () => {
-    assert.file(expectedFilePaths);
+    assert.file(absPaths(expectedFilePaths));
   });
 
   it('doesn\'t create unexpected files', () => {
     const actualFileSet = new Set(
       normalizePathSeparators(
-        walkSync('.', [], 'file')));
+        walkSync('', process.cwd(), [], 'file')));
     const expectedFileSet = new Set(expectedFilePaths);
     assert.equal(
       difference(actualFileSet, expectedFileSet).size,
@@ -129,13 +157,13 @@ describe('generator-python-cmd:app', () => {
   });
 
   it('creates expected empty directories', () => {
-    assert.file(expectedEmptyDirPaths);
+    assert.file(absPaths(expectedEmptyDirPaths));
   });
 
   it('doesn\'t create unexpected empty directories', () => {
     const actualEmptyDirSet = new Set(
       normalizePathSeparators(
-        walkSync('.', [], 'emptyDir')));
+        walkSync('', process.cwd(), [], 'emptyDir')));
     const expectedEmptyDirSet = new Set(expectedEmptyDirPaths);
     assert.equal(
       difference(actualEmptyDirSet, expectedEmptyDirSet).size,
@@ -162,7 +190,7 @@ describe('generator-python-cmd:app', () => {
         });
 
       fileContentItems.push(
-        [expectedFilePath, expectedContent]);
+        [absPath(expectedFilePath), expectedContent]);
     }
 
     assert.fileContent(fileContentItems);
