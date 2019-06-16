1e3ecb9e628bdae8d4e92b4c2cfc1656ed6fb298
Andy Culbertson <thinkulum@gmail.com>
2019-06-15 22:27:43 -0500

    Simplify the config.py template
    I'll add to it again once I decide what should be there. The existing code was half baked, but the tests weren't running it.

--- generators/app/templates/package/controller.py
+++ generators/app/templates/package/controller.py
@@ -1,12 +1,13 @@
-import cmd
+from . import config
 
 
 class Command(object):
     """
     Superclass for commands.
     """
+
     def __init__(self, arg):
         super(Command, self).__init__()
         self.arg = arg
-        
+
         self.config = config.Config()
